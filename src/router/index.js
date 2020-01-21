d5da5aa53222939dd7c0fb0d28061b8c07ac4d75
customer-w <1042897177@qq.com>
2020-01-21 19:27:20 +0800

    ''

---
+++ src/router/index.js
@@ -0,0 +1,11 @@
+import vue from 'vue'
+import Vuerouter from 'vue-router'
+vue.use(Vuerouter)
+const route =[
+
+]
+const router = new Vuerouter({
+    route,
+    mode:'history'
+})
+export default router
